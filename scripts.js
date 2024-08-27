f18f7b66067e09f9608ba4db18268f39a72dd913
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-08-27 20:43:42 -0300

    vinculação de eventos a um elemento html

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 const p = document.getElementById("par");
 p.innerText = "Troquei o texto do parágrafo";
+
+const p2 = document.getElementById("p2");
+
+p2.onclick = () => {
+  console.log("clicou em p2");
+};
+
+p2.addEventListener("click", () => console.log("evento do listener"));
+
+p2.addEventListener("dblclick", () => alert("segundo alerta!!!"));
