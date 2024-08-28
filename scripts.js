218835b141168b4b97c7e9690003b2e37c97eb45
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-08-27 22:28:07 -0300

    aula 6

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 const p = document.getElementById("par");
-p.innerText = "Troquei o texto do parágrafo";
+p.innerText = "Novo texto do parágrafo!";
+
+function bla() {
+  console.log("bla");
+}
 
 const p2 = document.getElementById("p2");
+p2.onclick = () => console.log("clicou no parágrafo 2!");
 
-p2.onclick = () => {
-  console.log("clicou em p2");
-};
-
-p2.addEventListener("click", () => console.log("evento do listener"));
-
-p2.addEventListener("dblclick", () => alert("segundo alerta!!!"));
+p2.addEventListener("dblclick", bla);
+p2.addEventListener("click", () => console.log('1'));
+p2.addEventListener("click", () => console.log('2'));
+p2.addEventListener("click", () => console.log('3'));
